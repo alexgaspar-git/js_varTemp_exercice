277d1ca893394c759ace9fcfedbadf5adc198a0a
Alex <agasparr98@gmail.com>
2021-01-05 10:57:05 +0100

    exo fini

---
+++ public/js/main.js
@@ -0,0 +1,113 @@
+// // - # Variables temporaires
+// // let a = 1; -> 2
+// // let b = 2; -> 3
+// // let c = 3; -> 1
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+
+// let temp;
+// temp = c;
+// c = a;
+// a = b;
+// b = temp;
+
+// //     - ## Afficher le changement des variables
+
+// console.log(a, b, c);
+
+// - # Exo 1
+    // - ## Afficher 4, 3, 2, 1
+// let a = 1; -> 4
+// let b = 2; -> 3
+// let c = 3; -> 2
+// let d = 4; -> 1
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let d = 4;
+
+// let temp;
+// temp = a;
+// a = d;
+// d = temp;
+// temp = b;
+// b = c;
+// c = temp;
+
+// //     - ## Afficher le changement des variables
+
+// console.log(a, b , c, d);
+
+// - #  Exo 2
+// let a = 1; -> 2
+// let b = 2; -> 3
+// let c = 3; -> 4
+// let d = 4; -> 1
+
+// //     - ## Afficher le changement des variables
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let d = 4;
+
+// let temp;
+
+// temp = a;
+
+// a = b;
+// b = c;
+// c = d;
+// d = temp;
+
+// console.log(a, b, c, d);
+
+// // - # Exo 3
+
+// let tableau = [1, 2, 3, 4];
+
+// let temp;
+// temp = tableau[0];
+// tableau[0] = tableau[3];
+// tableau[3] = temp;
+// temp = tableau[1];
+// tableau[1] = tableau[2];
+// tableau[2] = temp;
+
+// // - ## Afficher 4, 3, 2, 1 avec une variable temporaire
+
+// console.log(tableau);
+
+// // - # Exo 4
+// let a = 1 -> 4
+// let b = 2 -> 5
+// let c = 3 -> 1
+// let d = 4 -> 2
+// let e = 5 -> 6
+// let f = 6 -> 3
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let d = 4;
+// let e = 5;
+// let f = 6;
+
+// let temp;
+// let temp1;
+
+// temp = c;
+// c = a;
+// a = d;
+// d = b;
+// b = e;
+// e = f;
+// f = temp;
+
+// console.log(a, b, c, d, e, f);
+
+
+    // - ## Afficher le changement des variables
